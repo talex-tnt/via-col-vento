dcba42e9071d0617b065bab62fd98644aab58bcc
Alex Tinti <alex.tinti@spindox.it>
2025-05-08 11:06:50 +0200

    feat: use resizable panels

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,3 +1,4 @@
+import { useMemo, useState, useEffect, useRef } from 'react';
 import {UnconnectedGlobalMenu} from './components/menus/global';
 import {Route, Router} from 'wouter';
 import PANES from './utils/pane-config';
@@ -6,10 +7,19 @@ import {createGlobalStyle} from 'styled-components';
 import {CanvasRouter as CanvasRouter3D} from './components/three-fiber/canvas-router';
 import {CanvasRouter as CanvasRouter2D} from './components/two-string/canvas-router';
 import {TestContext} from './components/panes/test';
-import {useMemo, useState} from 'react';
 import {OVERRIDE_HID_CHECK} from './utils/override';
 import {useAppSelector} from './store/hooks';
 import {getRenderMode} from './store/settingsSlice';
+import {useCanvasConfig} from 'src/utils/canvas';
+
+import {
+  Panel,
+  PanelGroup,
+  PanelResizeHandle,
+  getPanelElement,
+  getPanelGroupElement,
+  getResizeHandleElement,
+} from "react-resizable-panels";
 
 const GlobalStyle = createGlobalStyle`
   *:focus {
@@ -19,6 +29,28 @@ const GlobalStyle = createGlobalStyle`
 
 export default () => {
   const hasHIDSupport = 'hid' in navigator || OVERRIDE_HID_CHECK;
+  const {hideCanvasScene, path, selectedDefinition} = useCanvasConfig();
+  const isConfigureRoute = path === '/';
+  const refs = useRef<{
+    topPanelElement: HTMLElement | null;
+    bottomPanelElement: HTMLElement | null;
+    resizeHandleElement: HTMLElement | null;
+  } | undefined>();
+
+  useEffect(() => {
+    const topPanelElement = getPanelElement("top-panel");
+    const bottomPanelElement = getPanelElement("bottom-panel");
+    const resizeHandleElement = getResizeHandleElement("resize-handle");
+    refs.current = {
+      topPanelElement,
+      bottomPanelElement,
+      resizeHandleElement,
+    };
+  }, []);
+
+
+
+  const canvasRouterHeight = (isConfigureRoute && !selectedDefinition) ? "50%" : "100%";
 
   const renderMode = useAppSelector(getRenderMode);
   const RouteComponents = useMemo(
@@ -30,8 +62,26 @@ export default () => {
       }),
     [],
   );
-  const CanvasRouter = renderMode === '2D' ? CanvasRouter2D : CanvasRouter3D;
+  const is2DRendering = renderMode === '2D';
+  const CanvasRouter = is2DRendering ? CanvasRouter2D : CanvasRouter3D;
   const testContextState = useState({clearTestKeys: () => {}});
+
+  useEffect(() => {
+    if(refs.current) {
+      const {topPanelElement, bottomPanelElement, resizeHandleElement} = refs.current;
+      if(topPanelElement && bottomPanelElement && resizeHandleElement) {
+        if(isConfigureRoute && !selectedDefinition) {
+          topPanelElement.style.display = is2DRendering ? "none": "block";
+          bottomPanelElement.style.display = !is2DRendering ? "none": "block";
+          resizeHandleElement.style.display = "none";
+        } else {
+          topPanelElement.style.display = hideCanvasScene ? "none" : "block";
+          bottomPanelElement.style.display = "block";
+          resizeHandleElement.style.display = hideCanvasScene ? "none" : "block";
+        }
+      }
+  }
+}, [hideCanvasScene, isConfigureRoute]);
   const base = import.meta.env.BASE_URL;
   return (
     <>
@@ -39,8 +89,17 @@ export default () => {
             <GlobalStyle />
             <Router base={base !== '/' ? base : undefined} >
               {hasHIDSupport && <UnconnectedGlobalMenu />}
-              <CanvasRouter height={700}/>
-              <Home hasHIDSupport={hasHIDSupport}>{RouteComponents}</Home>
+
+              <PanelGroup direction="vertical">
+                <Panel id="top-panel">
+                  <CanvasRouter height={canvasRouterHeight} />
+                </Panel>
+                <PanelResizeHandle  id="resize-handle"/>
+                <Panel id="bottom-panel">
+                  <Home hasHIDSupport={hasHIDSupport}>{RouteComponents}</Home>
+                </Panel>
+              </PanelGroup>
+
             </Router>
         </TestContext.Provider>
     </>
