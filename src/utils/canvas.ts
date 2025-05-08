ac9d8267fcb77946ad6a30469534d4460d661be5
Alex Tinti <alex.tinti@spindox.it>
2025-05-08 11:06:33 +0200

    chore: refactor canvas config

--- src/utils/canvas.ts
+++ src/utils/canvas.ts
@@ -1,3 +1,49 @@
+
+
+import {useProgress} from '@react-three/drei';
+import {DefinitionVersionMap} from '@the-via/reader';
+import {
+  getCustomDefinitions,
+  getSelectedDefinition,
+} from 'src/store/definitionsSlice';
+import {useAppSelector} from 'src/store/hooks';
+import {getLoadProgress} from 'src/store/keymapSlice';
+import {getDesignDefinitionVersion} from 'src/store/settingsSlice';
+import {OVERRIDE_HID_CHECK} from 'src/utils/override';
+import {useLocation} from 'wouter';
+import {useMemo} from 'react';
+
+export const useCanvasConfig = () => {
+	const [path] = useLocation();
+	const loadProgress = useAppSelector(getLoadProgress);
+	const {progress} = useProgress();
+	const localDefinitions = Object.values(useAppSelector(getCustomDefinitions));
+	const selectedDefinition = useAppSelector(getSelectedDefinition);
+	const definitionVersion = useAppSelector(getDesignDefinitionVersion);
+	const versionDefinitions: DefinitionVersionMap[] = useMemo(
+		() =>
+		localDefinitions.filter(
+			(definitionMap) => definitionMap[definitionVersion],
+		),
+		[localDefinitions, definitionVersion],
+	);
+	const hideDesignScene = '/design' === path && !versionDefinitions.length;
+	const hideConfigureScene =
+		'/' === path &&
+		(!selectedDefinition || (loadProgress + progress / 100) / 2 !== 1);
+	const showAuthorizeButton = 'hid' in navigator || OVERRIDE_HID_CHECK;
+	const hideCanvasScene =
+		!showAuthorizeButton ||
+		['/settings', '/errors', '/debug'].includes(path) ||
+		hideDesignScene ||
+		hideConfigureScene;
+
+		console.log({hideCanvasScene, showAuthorizeButton, path, hideDesignScene, hideConfigureScene, selectedDefinition, loadProgress, progress});
+
+	return { hideCanvasScene, showAuthorizeButton, path, selectedDefinition };
+}
+
+
 export function fitTextToWidth(ctx: CanvasRenderingContext2D, text: string, maxWidth: number, initialFontSize = 32, minFontSize = 8, fontFamily = 'Arial', bold = true) {
 	let fontSize = initialFontSize;
 	let lastWidth = Infinity;
